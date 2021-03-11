b2e0f91b67a91ef5ad6fbebdb41f43baf5d9d082
damien <damien0325@gmail.com>
2021-03-11 20:59:10 +0800

    simplify splittable

--- simulation/src/main/kotlin/com/noodle/datastructure/IBarnesHutTree.kt
+++ simulation/src/main/kotlin/com/noodle/datastructure/IBarnesHutTree.kt
@@ -6,7 +6,6 @@ interface IBarnesHutTree<T> :
         ISplittableTree<T>,
         IPointMass,
         Iterable<IBarnesHutTree<T>>{
-    override fun iterator(): Iterator<IBarnesHutTree<T>>
     fun force(node: IBarnesHutTree<T>, exponent: Int, theta: Double = 0.5): List<Double>
     fun occupancy(): Long
 }
